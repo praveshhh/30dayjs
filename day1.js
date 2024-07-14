20639bb4ab9d82d5024df88e2effbf5e7568ed70
praveshhh <83650338+praveshhh@users.noreply.github.com>
2024-07-14 16:31:11 +0000

    chai code js challange day1 config done!

---
+++ day1.js
@@ -0,0 +1,29 @@
+//task 1
+var mytea1=7;
+let tea2 = "Chai";
+console.log(mytea1);
+console.log(tea2);
+//task 2
+ const boolean1= true;
+ console.log(boolean1); 
+
+ //task 3
+ var num1= 1234;
+ var string1= "sql";
+ var boolean= true;
+ var object1= {name:"batman", power:"rich"}
+ var array1=[1,2,3,4,5];
+ console.log(typeof num1);
+ console.log(typeof string1);
+ console.log(typeof boolean);
+ console.log(typeof object1)
+ console.log(typeof array1); 
+
+ //task 4 
+ let tea3= "relaxing";
+ tea3= 10;
+ console.log(tea3);
+
+ const tea3="amazing";
+
+
