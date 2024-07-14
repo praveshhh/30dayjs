e0a3f914395ee753f59c13da363df4c6ad41821e
praveshhh <83650338+praveshhh@users.noreply.github.com>
2024-07-14 19:39:32 +0000

    30 days of js by chai code day2 config done

---
+++ day2.js
@@ -0,0 +1,62 @@
+//task1
+let num1= 10;
+let num2=5;
+let sum=num1+num2;
+console.log("SUM :" + sum);
+
+//task 2
+subtraction= num1-num2;
+console.log("SUBTRACT:"+subtraction);
+
+//task3
+multiplication= num1*num2;
+console.log("MULTIPLICATION:"+multiplication);
+
+//task 4
+division= num1/num2;
+console.log("DIVISION:"+division);
+
+//task 5
+rem=num1%num2;
+console.log("REMAINDER:"+rem);
+
+//task 6
+sum += num1
+console.log("+=Sum (sum=sum+num1// 15+10): " + sum);
+//task 7
+subtraction -=num1
+console.log("-=num1:sub-num1=sub:5-10=-5:"+subtraction);
+
+//task 8
+num1>num2;
+num1<num2;
+console.log(num1>num2);
+console.log(num1<num2);
+//task 9
+console.log(num1>=num2);
+console.log(num1<=num2);
+
+//task 10
+console.log(1=="1");
+console.log(typeof 4==="4");
+
+//task 11
+console.log("True && True: " + (2 < 3) && (4 > 2));
+console.log("True && False: " + (2 < 3) && (4 < 2));
+
+// Task 12:
+console.log("True ||  True: " + (2 < 3) || (4 > 2));
+console.log("True || False: " + (2 < 3) || (4 < 2));
+
+// Task 13:
+console.log(4 != 5);
+console.log(4 != 4);
+
+// Activity 5:
+// Task 14:
+a = 23;
+b = 2;
+
+a % b == 0 ? console.log("a%b==0") : console.log("a%b!=0");
+
+
