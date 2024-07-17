6c4d8e6ce2e086cad01f10575d3ef88bc45b797c
praveshhh <83650338+praveshhh@users.noreply.github.com>
2024-07-17 13:32:04 +0000

    task 1 and task2 config done

---
+++ day4.js
@@ -0,0 +1,18 @@
+//task1
+
+function task1(){
+    for(let i=1; i<=10; i++)
+    console.log(i); 
+}
+task1 ();
+
+//task 2
+
+function task2(){
+    for(let i=1; i<=10;i++){
+        console.log(`5 X ${i}=${5*i}`);
+    }
+
+}
+task2();
+
