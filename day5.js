91bb1c5708e6b34c7690b45b4cac310c83431d76
praveshhh <83650338+praveshhh@users.noreply.github.com>
2024-07-20 08:40:09 +0000

    done till task3 day5 of 30dayjs

---
+++ day5.js
@@ -0,0 +1,34 @@
+//task 1
+
+function task1(number){
+if (number%2===0){
+    console.log("even number");
+}
+else{
+    console.log("odd number");
+}
+}
+task1(2);
+
+//task2
+function task2(number){
+   let num= number*number;
+   console.log("square of the number is: "+num);
+
+}
+task2(13);
+
+//task3
+
+function task3(){
+    let a=10;
+    let b=12;
+    if(a>b){
+        console.log("a is bigger number ");
+    }else{
+        console.log("b is bigger");
+    }
+}
+task3();
+
+//task4
