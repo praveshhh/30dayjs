43cb365fc9db109854e65a21f0d464a5d9e6eb57
praveshhh <83650338+praveshhh@users.noreply.github.com>
2024-07-28 15:12:06 +0000

    day6 of 30daysof-javascriptchallenge activity 2,3 config done

--- day6.js
+++ day6.js
@@ -1,3 +1,29 @@
+
+////////////Activity 1/////////////
 //task 1
 let  arr=[1,2,3,4,5,6,7];
 console.log(arr);
+
+//task2
+
+let arr1=[10,9,8,7];
+console.log(arr1[3]);
+console.log(arr1[0]);
+
+/////////activity 2///////
+//task 3
+let arr2=[11,12,13,14];
+arr2.push(16);
+console.log(arr2);
+
+//task4
+arr2.pop();
+console.log(arr2);
+
+//task5
+arr2.shift();
+console.log(arr2);
+
+//task6 
+arr2.unshift(0,16);
+console.log(arr2);
